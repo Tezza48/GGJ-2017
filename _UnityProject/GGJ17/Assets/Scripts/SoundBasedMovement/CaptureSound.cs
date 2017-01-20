0a475facde86e09b718bdae721ad906d4b873251
paperjuice <paperjuiceinc@gmail.com>
2017-01-20 22:50:54 +0000

    Scripts folder added to _UnityProject

---
+++ _UnityProject/GGJ17/Assets/Scripts/SoundBasedMovement/CaptureSound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureSound : MonoBehaviour {
+
+
+	AudioSource _audio;
+    float secondsHold;
+    string microphonesName;
+
+
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+	void Start()
+	{
+        foreach (string device in Microphone.devices)
+        {
+            if (microphonesName == null)
+                microphonesName = device;
+        }
+        //print();
+
+        _audio.mute = true;
+        _audio.loop = true;
+    }
+
+
+    void Update()
+    {
+
+
+
+        if (Input.GetKey(KeyCode.Mouse0) && !(Microphone.GetPosition("device1") > 0))
+        {
+            secondsHold += Time.deltaTime;
+            print(secondsHold);
+            _audio.clip = Microphone.Start("device1", true, 10, 44100);
+        }
+        else
+        {
+            secondsHold = 0f;
+            Microphone.End("device1");
+        }
+        
+    }
+}

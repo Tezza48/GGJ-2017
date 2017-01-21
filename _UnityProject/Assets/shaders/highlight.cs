1c234093c461f7ef5673b30fd68c04244068619f
Samuel Cook <s172776@ucs.ac.uk>
2017-01-21 13:24:57 +0000

    New Level Added From Memory Stick

---
+++ _UnityProject/Assets/shaders/highlight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class highlight : MonoBehaviour {
+	public Color hightlight=Color.green;
+	public Color norm;
+
+	public Material[] mats;
+
+	void Start(){
+		mats = gameObject.GetComponent<Renderer> ().materials;
+	
+		norm = mats [0].color;
+	}
+
+		void OnTriggerEnter(Collider other) {
+			if (other.tag == "Player") {
+				mats [0].color = hightlight;
+			}
+		}
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			mats [0].color = norm;
+		}
+	}
+}

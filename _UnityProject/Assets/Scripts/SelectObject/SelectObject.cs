db39692497d97b7f96cd4146246882be1611c546
paperjuice <paperjuiceinc@gmail.com>
2017-01-21 04:04:47 +0000

    Working on cotrollers behaviour

---
+++ _UnityProject/Assets/Scripts/SelectObject/SelectObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour {
+
+
+    GameObject[] movableObject;
+    bool isFree = false;
+    float currentTime = 0f;
+    float endTime = 1f;
+
+    private void OnMouseOver()
+    {
+         gameObject.tag = "active";
+    }
+    private void OnMouseExit()
+    {
+        gameObject.tag = "Untagged";
+    }
+
+    
+
+
+
+}
--- _UnityProject/Assets/Scripts/SoundBasedMovement/CaptureSound.cs
+++ _UnityProject/Assets/Scripts/SoundBasedMovement/CaptureSound.cs
@@ -4,47 +4,111 @@ using UnityEngine;
 
 public class CaptureSound : MonoBehaviour {
 
+    [SerializeField]float intesity;
+    [SerializeField]float threshold = 0.85f;
+    [SerializeField] FFTWindow _fftWindows;
+    GameObject player;
 
+    //ActiveObject
+    [Header("Active Object")]
+    [SerializeField]float normalForceMultiplier;
+    Rigidbody activeObj;
+
+    //catapulte mode
+    [Header("Catapulte Volume Level")]
+    [SerializeField]float catapulteForceMultiplier;
+    [SerializeField]float catapultVolumeLevel;
+    bool isCatapulteMode;
+
+    //AudioShitNooneUnderstands
 	AudioSource _audio;
     float secondsHold;
     string microphonesName;
+    float force = 0f;
+    public float Force
+    {
+        get{return force; }
+        set { force = value;}
+    }
+    float[] spectrum = new float[256];
 
 
     void Awake()
     {
         _audio = GetComponent<AudioSource>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
 	void Start()
 	{
+        _audio.Stop();
+        _audio.clip = Microphone.Start(microphonesName, true, 10, 44100);
+        _audio.loop = true;
+
         foreach (string device in Microphone.devices)
-        {
             if (microphonesName == null)
                 microphonesName = device;
-        }
-        //print();
 
-        _audio.mute = true;
-        _audio.loop = true;
+        if (Microphone.IsRecording(microphonesName))
+        {
+            while (!(Microphone.GetPosition(microphonesName) > 0)) { }
+            _audio.Play();
+        }
+        else
+        {
+            print(microphonesName + "doesn't work");
+        }        
     }
 
 
     void Update()
     {
-
-
-
-        if (Input.GetKey(KeyCode.Mouse0) && !(Microphone.GetPosition("device1") > 0))
-        {
-            secondsHold += Time.deltaTime;
-            print(secondsHold);
-            _audio.clip = Microphone.Start("device1", true, 10, 44100);
-        }
-        else
-        {
-            secondsHold = 0f;
-            Microphone.End("device1");
-        }
-        
+        FromSoundToForce();
     }
+
+
+    void FromSoundToForce()
+    {
+        _audio.GetSpectrumData(spectrum, 0, _fftWindows);
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            if (spectrum[i] > threshold)
+            {
+                //we get all intensities in spectrum
+                intesity = spectrum[i] * 100f;
+
+                //save the highest value
+                if (Force < intesity)
+                    Force = intesity;
+            }
+            else
+            {
+                //when intensity is past threshold we reset the max volumes
+                Force = 0;
+            }
+
+
+        }
+
+    }
+
+
+
+
+
+
+    //void Levitate(Rigidbody activeObject, float _force)
+    //{
+    //    if (activeObj != null)
+    //        activeObject.AddForce(_force * Time.deltaTime * Vector3.up * normalForceMultiplier);
+    //}
+
+
+    //void CatapulteMode(Rigidbody activeObject, float _force)
+    //{
+    //    if (activeObj != null)
+    //    {
+    //        activeObject.AddForce(_force * Time.deltaTime * new Vector3(0f, 1f, 0f) + (activeObj.transform.position - player.transform.position).normalized * catapulteForceMultiplier);
+    //    }
+    //}
 }

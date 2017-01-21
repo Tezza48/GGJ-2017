a913bfdbf31cf59d8ac2995b0d80c011780975f4
Samuel Cook <s172776@ucs.ac.uk>
2017-01-21 16:10:41 +0000

    Object respawning and Sound movement tweaks

--- _UnityProject/Assets/Scripts/Landmass.cs
+++ _UnityProject/Assets/Scripts/Landmass.cs
@@ -6,7 +6,9 @@ public class Landmass : MonoBehaviour
 {
 
     [SerializeField][Tooltip("Bitflag for this landmas's tag. \nShould be power of 2")]
-    private int landmassFlag;
+    private int landmassFlag = 0;
 
     public int LandmassFlag { get { return landmassFlag; } }
+
+
 }

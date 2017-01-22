c1dfbfca8c33a79278f794514fc71bf13dca9e75
paperjuice <paperjuiceinc@gmail.com>
2017-01-22 14:46:30 +0000

    VolumeMeasurement.cs

---
+++ _UnityProject/Assets/Scripts/LevelUtility/VolumeMeasurement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeMeasurement : MonoBehaviour {
+
+
+    float force;
+    ParticleSystem _particle;
+    CaptureSound _capture;
+
+
+
+    private void Awake()
+    {
+        _capture = GameObject.Find("GamePlayerSettings").GetComponent<CaptureSound>();
+        _particle = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        force = _capture.Force;
+        Debug.Log(force);
+        var ma = _particle.main;
+        ma.startSize = force/10;
+    }
+
+
+
+
+
+}

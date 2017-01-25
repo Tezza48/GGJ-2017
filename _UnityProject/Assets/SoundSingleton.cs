8fad3eaa09243a955157e1e2c5943644002c671b
William terry <tezza48is@aol.com>
2017-01-25 09:08:31 +0000

    Build and finished game. :godmode:
    Good job Chris didn't reinstall.

---
+++ _UnityProject/Assets/SoundSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSingleton : MonoBehaviour
+{
+
+    public static SoundSingleton singleton;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ _UnityProject/Assets/WallLevelParentRemover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WallLevelParentRemover : MonoBehaviour
+{
+
+    [SerializeField] GameObject img;
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "wall")
+        {
+            transform.parent = null;
+            GetComponent<Rigidbody>().isKinematic = false;
+            StartCoroutine(asd());
+        }
+    }
+
+
+    IEnumerator asd()
+    {
+        img.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Level 5 - Wall");
+
+    }
+}

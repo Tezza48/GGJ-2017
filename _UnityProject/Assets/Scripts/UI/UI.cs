8045f93af1c38845449895320c43c14653bdf607
Samuel Cook <s172776@ucs.ac.uk>
2017-02-09 11:37:48 +0000

    added ui scene and started building ui

---
+++ _UnityProject/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+void OnCollisionEnter2D (Collision2D coll) {
+        if (coll.gameObject.tag == "Start")
+            Debug.Log("start");
+    }
+}
